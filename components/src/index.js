10897275d6f32b6334264806dd49b46cbf4a1f70
movingbro <54231339+blivinme@users.noreply.github.com>
2020-12-06 23:33:54 +0900

    12/6

--- components/src/index.js
+++ components/src/index.js
@@ -6,10 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content="Nice blog Post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        content="서로 이웃 추가해요!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="비밀 댓글입니다."
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
